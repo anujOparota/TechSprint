71dc4c4359371a4de4a192120a53fafc481af15e
Suyash-1-hash <pawarsuyash260506@gmail.com>
2025-12-26 13:29:35 +0530

    ChatBot folder added - Suyash

---
+++ src/pages/Medicine/ChatBot/main.py
@@ -0,0 +1,62 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from groq import Groq
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+
+client = Groq(
+        api_key=os.environ.get("GROQ_API_KEY"),
+    )
+
+class chatRequest(BaseModel):
+    message : str
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True
+)
+
+def get_bot_response(user_message: str):
+    chat_completion = client.chat.completions.create(
+        model="llama-3.3-70b-versatile",
+        messages=[
+            {
+                "role": "system",
+                "content": (
+                    "You are a medical AI assistant. Be polite while answering."
+                    "If user greets you with hi, hello or anyother way reply with - Hello, I am Turbo Techies's AI medical AI assistant. I can help with questions related to medicines, health, and medical conditions. How can I assist you today?"
+                    "Do NOT use markdown for bold (**), italics (*) you can use numbers or bullets"
+                    "You must ONLY answer questions related to medicine, health, "
+                    "drugs, symptoms, diseases, or treatments. "
+                    "If the question is NOT medical-related, reply with: "
+                    "'I can only help with medical-related questions. For more info contact: +91 8104884493'"
+                )
+            },
+            {
+                "role": "user",
+                "content": user_message
+            }
+        ],
+        max_tokens=150,     
+        temperature=0.3,      
+        stream=False
+    )
+
+    return chat_completion.choices[0].message.content
+
+
+
+@app.post("/chat")
+async def chat(request:chatRequest):
+    reply=get_bot_response(request.message)
+    return {"reply":reply}
+    
